d244433af701c559fa712e39086d8c028ec95588
Anatolii K <tol890@ukr.net>
2020-10-18 10:30:14 +0300

    grab data from input forms (title and date)

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdowndate = '';
+
 // Set Date Input Minimum with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take Values From form Input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
